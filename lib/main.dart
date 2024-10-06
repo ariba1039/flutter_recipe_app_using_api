c89aa1513eb484cc413a6797d796e9fc1d207638
Ariba Hussain <aribahussain970@gmail.com>
2024-10-06 09:48:50 -0700

    fixes:done

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app_using_api/screens/recipe_main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: 'Flutter Recipe App',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: RecipesHomeScreen()
+    );
+  }
+}
---
+++ lib/screens/recipe_home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RecipeHomeScreen extends StatefulWidget {
+  const RecipeHomeScreen({super.key});
+
+  @override
+  State<RecipeHomeScreen> createState() => _RecipeHomeScreenState();
+}
+
+class _RecipeHomeScreenState extends State<RecipeHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipe App using Rest Apis'),
+      )
+    );
+  }
+}
